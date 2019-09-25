91c2687c6cb315ef8df4441645a0a2f3f3fe39cd
Tanzeel Rehman <tanzeel.rehman@solarwinds.com>
2019-09-25 07:15:45 -0400

    remove dashboard

--- index.js
+++ index.js
@@ -55,33 +55,3 @@ var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {  
     console.log('server running on port ' + port + '.');
 });
-
-// Set up parse dashboard
-var dashboard = new ParseDashboard({
-  "allowInsecureHTTP": true,
-  "apps": [{
-      "serverURL": process.env.SERVER_URL || 'https://pacecouriers.com/mufeezBhaiApp',
-      "appId": process.env.APP_ID || 'mufeezBhaiApp',
-      "masterKey": process.env.MASTER_KEY || 'mufeezBhaiApp',
-      "appName": "Mufeez Bhai App",
-      "production": false,
-      "iconName": "app-icon.png",
-  }],
-  "users": [
-    {
-      "user":"username",
-      "pass":"password"
-    }
-  ],
-  "iconsFolder": "icons"
-});
-
-var dashApp = express();
-
-// make the Parse Dashboard available at /dashboard
-dashApp.use('/dashboard', dashboard);  
-
-var httpServerDash = require('http').createServer(dashApp);  
-httpServerDash.listen(4040, function() {  
-    console.log('dashboard-server running on port 4040.');
-});
