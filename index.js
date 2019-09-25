9758c754872cb709fdb4b42ce10ec324d3dfc5cb
Tanzeel Rehman <tanzeel.rehman@solarwinds.com>
2019-09-25 07:40:54 -0400

    remove Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -43,12 +43,6 @@ var api = new ParseServer({
 
 var app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
